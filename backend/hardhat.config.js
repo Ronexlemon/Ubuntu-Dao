cacd6f83bf3918f0aa97979af86e561ec03d949b
RonexLemon <ronexondimu@gmail.com>
2023-02-19 14:07:24 +0300

    configure hardhat

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({path:".env"});
+const KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks:{
+    fantomtest:{
+      url: "https://rpc.testnet.fantom.network",
+      accounts: [KEY],
+      chainId: 0xfa2,
+    },
+    
+  }
 };
