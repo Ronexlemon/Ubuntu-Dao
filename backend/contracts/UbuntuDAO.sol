9f0fa75b111027b2e4afd0f10b3101f70086a22a
RonexLemon <ronexondimu@gmail.com>
2023-02-19 13:54:57 +0300

    add openzepplin lib

--- backend/contracts/UbuntuDAO.sol
+++ backend/contracts/UbuntuDAO.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract UbuntuDAO {
     IERC20 UbuntuDaoTokens;
