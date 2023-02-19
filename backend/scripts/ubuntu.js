8d100e433952e1e8453e715cdcb9d3a7accbbe35
RonexLemon <ronexondimu@gmail.com>
2023-02-19 13:57:21 +0300

    initialize deployment script

---
+++ backend/scripts/ubuntu.js
@@ -0,0 +1,15 @@
+const {ethers} = require("hardhat")
+
+async function main(){
+    //get the contract
+    //deploy contract
+    //await deployment
+    //console.address
+}
+//call main
+main().then(()=>
+process.exit(0))
+.catch((error)=>{
+    process.exit(1);
+    console.log(error);
+})
