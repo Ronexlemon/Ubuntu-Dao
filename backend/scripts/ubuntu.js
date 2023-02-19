6e5943b5df2be6652523ce13a8e9276b91cd8ec7
RonexLemon <ronexondimu@gmail.com>
2023-02-19 15:34:56 +0300

    add addresses

--- backend/scripts/ubuntu.js
+++ backend/scripts/ubuntu.js
@@ -1,5 +1,6 @@
 const {ethers} = require("hardhat")
-
+// UbuntuTokenContractAddress: 0x05198c2783d3497361ca936a70E5643287dfD0B8
+// UbuntuDAOContractAddress: 0xE50A2E68f31e899D6e794314823cD2ac126BD764
 async function main(){
     //get the contract for tokens
     const UbuntuTokenContract = await ethers.getContractFactory("UbuntuTokens");
