784e32923390b6bfbc80ee37c46aa5618fb59d77
RonexLemon <ronexondimu@gmail.com>
2023-03-02 17:31:52 +0300

    check only the owner can mint

--- backend/test/ubuntutest.js
+++ backend/test/ubuntutest.js
@@ -3,7 +3,10 @@ const {expect} = require("chai")
 describe("deploy Contracts", function(){
     let ubuntuTokenContract;
     let ubuntuDaoContract;
+    let owner;
+    let otherAccount;
     before(async function(){
+        
         //deploy ubuntu token
         const ubuntuToken = await ethers.getContractFactory("UbuntuTokens");
       ubuntuTokenContract = await ubuntuToken.deploy();
@@ -12,13 +15,23 @@ describe("deploy Contracts", function(){
     const ubuntudao = await ethers.getContractFactory("UbuntuDAO")
      ubuntuDaoContract = await ubuntudao.deploy();
     await ubuntuDaoContract.deployed();
+    //get accounts
+    [owner, otherAccount] = await ethers.getSigners();
 
 
     })
 it("should deploy and return total supply to 0", async function(){
    const result = await ubuntuTokenContract.totalSupply();
-    
+   
     expect(result).to.equal(0);
+});
+it("should mint tokens to address", async function(){
+    const value = BigInt(1000);
+    await ubuntuTokenContract.connect(owner).mintTokens(ubuntuDaoContract.address, value);
+
+    // Check that the total supply has increased
+    const result = await ubuntuTokenContract.totalSupply();
+    expect(await result).to.equal(BigInt(1000*10**18) );
 })
 
 })
