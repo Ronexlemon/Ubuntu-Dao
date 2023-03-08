b896de0e5d7d304aed4f7031ef88acf0572722e8
anthonykimani <kimaniantoni@gmail.com>
2023-03-08 12:17:47 +0300

    Cleans up footer, adds responsiveness , tweaks create-posts page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,6 @@ import About from "../components/About";
 import Footer from "../components/Footer";
 import Join from "../components/Join";
 import LandingPage from "../components/LandingPage";
-import NewFooter from "../components/NewFooter";
 
 const Home = () => {
   return (
@@ -10,8 +9,7 @@ const Home = () => {
       <LandingPage />
       <About />
 
-      <NewFooter />
-      {/* <Footer /> */}
+      <Footer />
     </main>
   );
 };
